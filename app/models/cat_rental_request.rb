54ac2f9b1be2f3605902ecb5bc01638270bf23d6
App Academy Student <student@appacademy.io>
2013-12-11 18:04:02 -0800

    Begin rental request implementation

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -1,5 +1,12 @@
 class CatRentalRequest < ActiveRecord::Base
-  attr_accessible :cat_id, :start_date, :end_date
+  attr_accessible :cat_id, :start_date, :end_date, :status
+  validates :cat_id, :start_date, :end_date, :status, :presence => true
+  validates :status, :inclusion => %w{PENDING APPROVED DENIED}
 
+  def overlapping_requests
+    CatRentalRequests.where('start_date > ?', )
+  end
 
+  def overlapping_approved_requests
+  end
 end
